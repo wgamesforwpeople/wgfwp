400ca14c06ec2ffc6aaeb334a420458cc35b859e
wgamesforwpeople <wgfwp_unblocked@outlook.com>
2026-05-03 18:05:12 -0400

    Remove stuff cuz changed mind about layout

--- dist/src/index.js
+++ dist/src/index.js
@@ -1,24 +1,2 @@
 // handles main page n stuff
 
-let mainhtml = document.getElementById("main");
-let games;
-let mainloaded = true;
-
-gamesbutton.onclick = function() {
-    document.getElementById("main").textContent = "";
-
-    document.getElementById("gaming").innerHTML = `
-        <button id="mainbutton">Main page</button>
-        <h1>W games for W people</h1>
-        <p>heres some games:</p>
-        <!-- games go here -->
-    `;
-    console.log("Games Loaded");
-};
-mainbutton.onclick = function() {
-    document.getElementById("games").textContent = "";
-    
-    document.getElementById("main").innerHTML = `${mainhtml}`;
-    console.log(mainhtml);
-}
-
